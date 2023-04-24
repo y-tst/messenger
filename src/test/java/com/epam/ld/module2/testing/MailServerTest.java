03fcfbbd45fc9cae384cb17e274d3863e901537f
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-04-24 21:11:14 +0300

    add TDD test for MailService

--- src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,4 +1,33 @@
 package com.epam.ld.module2.testing;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MailServerTest {
+
+    private MailService mailService;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        mailService = new MailService();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void testSendMail() {
+        String to = "john@example.com";
+        String subject = "Test Subject";
+        String body = "Test Body";
+        String expectedOutput = "Mail sent to: " + to + "\nSubject: " + subject + "\nBody: " + body;
+
+        mailService.sendMail(to, subject, body);
+
+        assertEquals(expectedOutput, outputStreamCaptor.toString().trim());
+    }
 }
