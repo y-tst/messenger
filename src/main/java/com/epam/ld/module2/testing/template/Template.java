842919edc839c91b87423b24f7ebdb1facb486ce
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-04-24 20:54:28 +0300

    Added Template andTemplateEngine implementation

--- src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -1,7 +1,28 @@
 package com.epam.ld.module2.testing.template;
 
-/**
- * The type Template.
- */
+import java.util.Map;
+
 public class Template {
+
+    private String templateText;
+
+    public Template(String templateText) {
+        this.templateText = templateText;
+    }
+
+    public String getTemplateText() {
+        return templateText;
+    }
+
+    public String replacePlaceholders(Map<String, String> values) throws RuntimeException {
+        String result = templateText;
+
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            String placeholder = "#{" + entry.getKey() + "}";
+            String value = entry.getValue();
+            result = result.replace(placeholder, value);
+        }
+
+        return result;
+    }
 }
--- src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
+++ src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
@@ -2,9 +2,8 @@ package com.epam.ld.module2.testing.template;
 
 import com.epam.ld.module2.testing.Client;
 
-/**
- * The type Template engine.
- */
+import java.util.Map;
+
 public class TemplateEngine {
     /**
      * Generate message string.
@@ -15,5 +14,14 @@ public class TemplateEngine {
      */
     public String generateMessage(Template template, Client client) {
         return null;
+    }
+
+    public String generateMessage(String templateText, Map<String, String> values) {
+        Template template = new Template(templateText);
+        if (templateText.chars().filter(ch -> ch == '#').count() <= values.size()) {
+            String message = template.replacePlaceholders(values);
+            return message;
+        }
+        else throw new RuntimeException("Missing values for template replacement");
     }
 }
