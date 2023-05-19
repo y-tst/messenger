368ac3a60a2fda87aaa6b5505cc557fd7aaed571
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-05-19 20:37:46 +0300

    uncomment needed code

--- src/main/java/com/epam/ld/module2/testing/Demo.java
+++ src/main/java/com/epam/ld/module2/testing/Demo.java
@@ -13,35 +13,35 @@ public class Demo {
 
         /* Using Console Input */
 
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.println("Type message template in format: 'Hello, #{name}! Your email is #{email}.',\n" +
-//                "(where #{...} - mandatory parameters, could be any number of them,\n" +
-//                "text could be due to your needs) end press 'Enter':");
-//        String customTemplate = scan.nextLine();
-//
-//        System.out.println("Type client's name:");
-//        String userName = scan.nextLine();
-//
-//        String userEmail;
-//
-//        do {
-//            System.out.println("Type correct client's email:");
-//            userEmail = scan.nextLine();
-//        }
-//        while (!EmailVerifier.isValidEmail(userEmail));
-//
-//
-//        Client client = new Client(userName, userEmail);
-//
-//        System.out.println("Sending email:\n" +
-//                "________________________________________________");
-//
-//        MailServer mailServer = new MailServer(new TemplateEngine(), client);
-//        Messenger messenger = new Messenger(mailServer, new TemplateEngine());
-//
-//        messenger.sendMessage(client, new Template(customTemplate));
-//
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Type message template in format: 'Hello, #{name}! Your email is #{email}.',\n" +
+                "(where #{...} - mandatory parameters, could be any number of them,\n" +
+                "text could be due to your needs) end press 'Enter':");
+        String customTemplate = scan.nextLine();
+
+        System.out.println("Type client's name:");
+        String userName = scan.nextLine();
+
+        String userEmail;
+
+        do {
+            System.out.println("Type correct client's email:");
+            userEmail = scan.nextLine();
+        }
+        while (!EmailVerifier.isValidEmail(userEmail));
+
+
+        Client client = new Client(userName, userEmail);
+
+        System.out.println("Sending email:\n" +
+                "________________________________________________");
+
+        MailServer mailServer = new MailServer(new TemplateEngine(), client);
+        Messenger messenger = new Messenger(mailServer, new TemplateEngine());
+
+        messenger.sendMessage(client, new Template(customTemplate));
+
 
         /* Using File processor */
 
